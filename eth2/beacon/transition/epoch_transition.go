6bb4259dab8b70e0b9e696dfb1e0f8e755446cdb
protolambda <proto@protolambda.com>
2019-03-15 08:39:36 +0100

    forgot to call 1 transition

--- eth2/beacon/transition/epoch_transition.go
+++ eth2/beacon/transition/epoch_transition.go
@@ -12,6 +12,7 @@ var epochProcessors = []EpochProcessor{
 	epoch_processing.ProcessEpochJustification,
 	epoch_processing.ProcessEpochCrosslinks,
 	epoch_processing.ProcessEpochRewardsAndPenalties,
+	epoch_processing.ProcessEpochEjections,
 	epoch_processing.ProcessEpochValidatorRegistry,
 	epoch_processing.ProcessEpochSlashings,
 	epoch_processing.ProcessEpochExitQueue,

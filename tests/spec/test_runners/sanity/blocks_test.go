0dec9fb85b455497bb99bbe5294693bf17ec6498
protolambda <proto@protolambda.com>
2019-05-31 01:49:42 +0200

    sanity test runner

---
+++ tests/spec/test_runners/sanity/blocks_test.go
@@ -0,0 +1,32 @@
+package sanity
+
+import (
+	"github.com/protolambda/zrnt/eth2/beacon"
+	"github.com/protolambda/zrnt/eth2/beacon/transition"
+	. "github.com/protolambda/zrnt/tests/spec/test_util"
+	"testing"
+)
+
+type BlocksTestCase struct {
+	Blocks             []*beacon.BeaconBlock
+	StateTransitionTestBase `mapstructure:",squash"`
+}
+
+func (testCase *BlocksTestCase) Process() error {
+	for _, block := range testCase.Blocks {
+		// TODO: mark block number in error? (Maybe with Go 1.13, coming out soon, supporting wrapped errors)
+		if err := transition.StateTransition(testCase.Pre, block, false); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (testCase *BlocksTestCase) Run(t *testing.T) {
+	RunTest(t, testCase)
+}
+
+func TestBlocks(t *testing.T) {
+	RunSuitesInPath("sanity/blocks/",
+		func(raw interface{}) interface{} { return new(BlocksTestCase) }, t)
+}
---
+++ tests/spec/test_runners/sanity/slots_test.go
@@ -0,0 +1,30 @@
+package sanity
+
+import (
+	"github.com/protolambda/zrnt/eth2/beacon/transition"
+	. "github.com/protolambda/zrnt/eth2/core"
+	. "github.com/protolambda/zrnt/tests/spec/test_util"
+	"testing"
+)
+
+type SlotsTestCase struct {
+	Slots             Slot
+	StateTransitionTestBase `mapstructure:",squash"`
+}
+
+func (testCase *SlotsTestCase) Process() error {
+	// TODO: mark block number in error? (Maybe with Go 1.13, coming out soon, supporting wrapped errors)
+	if err := transition.StateTransitionTo(testCase.Pre, testCase.Pre.Slot + testCase.Slots); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (testCase *SlotsTestCase) Run(t *testing.T) {
+	RunTest(t, testCase)
+}
+
+func TestSlots(t *testing.T) {
+	RunSuitesInPath("sanity/slots/",
+		func(raw interface{}) interface{} { return new(SlotsTestCase) }, t)
+}

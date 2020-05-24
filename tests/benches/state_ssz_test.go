88e4d5231b22c5a9c45f89e3e5e4382b19dafb3c
protolambda <proto@protolambda.com>
2020-05-24 16:32:52 +0200

    update benchmark code

--- tests/benches/state_ssz_test.go
+++ tests/benches/state_ssz_test.go
@@ -2,50 +2,92 @@ package benches
 
 import (
 	"bytes"
-	//"crypto/sha256"
 	"encoding/gob"
 	"github.com/minio/sha256-simd"
-	. "github.com/protolambda/zrnt/eth2/core"
-	"github.com/protolambda/zrnt/eth2/phase0"
+	. "github.com/protolambda/zrnt/eth2/beacon"
 	"github.com/protolambda/zrnt/eth2/util/ssz"
 	"github.com/protolambda/zssz"
+	"github.com/protolambda/ztyp/tree"
 	"testing"
 )
 
 const stateValidatorFill = 300000
 
-func BenchmarkStateHash(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+func BenchmarkTreeStateHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	hFn := tree.GetHashFn()
+	b.ReportAllocs()
 	b.ResetTimer()
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
-		root := ssz.HashTreeRoot(full.BeaconState, phase0.BeaconStateSSZ)
-		res ^= root[0]
+		root := stateTree.HashTreeRoot(hFn)
+		res += root[0]
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
 
-func BenchmarkFlatHash(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+func BenchmarkTreeStateFlatHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
 	h := sha256.New()
 	b.ReportAllocs()
 	b.ResetTimer()
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
-		_, err := zssz.Encode(h, full.BeaconState, phase0.BeaconStateSSZ)
+		err := stateTree.Serialize(h)
 		if err != nil {
 			b.Fatal(err)
 		}
-		res ^= h.Sum(nil)[0]
+		res += h.Sum(nil)[0]
 		h.Reset()
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
+}
+
+func BenchmarkRawStateHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	res := byte(0)
+	for i := 0; i < b.N; i++ {
+		root := ssz.HashTreeRoot(state, BeaconStateSSZ)
+		res += root[0]
+	}
+	//b.Logf("res: %d, N: %d", res, b.N)
+}
+
+func BenchmarkRawStateFlatHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
+	h := sha256.New()
+	b.ReportAllocs()
+	b.ResetTimer()
+	res := byte(0)
+	for i := 0; i < b.N; i++ {
+		_, err := zssz.Encode(h, state, BeaconStateSSZ)
+		if err != nil {
+			b.Fatal(err)
+		}
+		res += h.Sum(nil)[0]
+		h.Reset()
+	}
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
 
 func BenchmarkStateNoEncodingFlatHash(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
 	var buf bytes.Buffer
-	_, err := zssz.Encode(&buf, full.BeaconState, phase0.BeaconStateSSZ)
+	_, err = zssz.Encode(&buf, state, BeaconStateSSZ)
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -56,63 +98,112 @@ func BenchmarkStateNoEncodingFlatHash(b *testing.B) {
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
 		h.Write(data)
-		res ^= h.Sum(nil)[0]
+		res += h.Sum(nil)[0]
 		h.Reset()
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
 
-func BenchmarkStateFullFlatHash(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+func BenchmarkTreeStateBufferedFlatHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
 	var buf bytes.Buffer
 	h := sha256.New()
 	b.ReportAllocs()
 	b.ResetTimer()
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
-		_, err := zssz.Encode(&buf, full.BeaconState, phase0.BeaconStateSSZ)
+		err := stateTree.Serialize(&buf)
 		if err != nil {
 			b.Fatal(err)
 		}
 		h.Write(buf.Bytes())
-		res ^= h.Sum(nil)[0]
+		res += h.Sum(nil)[0]
 		h.Reset()
 		buf.Reset()
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
 
-func BenchmarkStateSerialize(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+func BenchmarkRawStateBufferedFlatHash(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
+	var buf bytes.Buffer
+	h := sha256.New()
+	b.ReportAllocs()
+	b.ResetTimer()
+	res := byte(0)
+	for i := 0; i < b.N; i++ {
+		_, err := zssz.Encode(&buf, state, BeaconStateSSZ)
+		if err != nil {
+			b.Fatal(err)
+		}
+		h.Write(buf.Bytes())
+		res += h.Sum(nil)[0]
+		h.Reset()
+		buf.Reset()
+	}
+	//b.Logf("res: %d, N: %d", res, b.N)
+}
+
+func BenchmarkRawStateSerialize(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
 	var buf bytes.Buffer
 	b.ReportAllocs()
 	b.ResetTimer()
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
-		_, err := zssz.Encode(&buf, full.BeaconState, phase0.BeaconStateSSZ)
+		_, err := zssz.Encode(&buf, state, BeaconStateSSZ)
 		if err != nil {
 			b.Fatal(err)
 		}
-		res ^= buf.Bytes()[0]
+		res += buf.Bytes()[0]
 		buf.Reset()
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
 
-func BenchmarkStateSerializeGob(b *testing.B) {
-	full := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+func BenchmarkTreeStateSerialize(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	var buf bytes.Buffer
+	b.ReportAllocs()
+	b.ResetTimer()
+	res := byte(0)
+	for i := 0; i < b.N; i++ {
+		err := stateTree.Serialize(&buf)
+		if err != nil {
+			b.Fatal(err)
+		}
+		res += buf.Bytes()[0]
+		buf.Reset()
+	}
+	//b.Logf("res: %d, N: %d", res, b.N)
+}
+
+func BenchmarkRawStateSerializeGob(b *testing.B) {
+	stateTree, _ := CreateTestState(stateValidatorFill, MAX_EFFECTIVE_BALANCE)
+	state, err := stateTree.Raw()
+	if err != nil {
+		b.Fatal(err)
+	}
 	var buf bytes.Buffer
 	g := gob.NewEncoder(&buf)
 	b.ReportAllocs()
 	b.ResetTimer()
 	res := byte(0)
 	for i := 0; i < b.N; i++ {
-		err := g.Encode(full.BeaconState)
+		err := g.Encode(state)
 		if err != nil {
 			b.Fatal(err)
 		}
-		res ^= buf.Bytes()[0]
+		res += buf.Bytes()[0]
 		buf.Reset()
 	}
-	b.Logf("res: %d", res)
+	//b.Logf("res: %d, N: %d", res, b.N)
 }
--- tests/benches/util.go
+++ tests/benches/util.go
@@ -2,14 +2,13 @@ package benches
 
 import (
 	"encoding/binary"
-	. "github.com/protolambda/zrnt/eth2/core"
-	. "github.com/protolambda/zrnt/eth2/phase0"
+	. "github.com/protolambda/zrnt/eth2/beacon"
 )
 
 func CreateTestValidators(count uint64, balance Gwei) []KickstartValidatorData {
 	out := make([]KickstartValidatorData, 0, count)
 	for i := uint64(0); i < count; i++ {
-		pubkey := BLSPubkeyNode{0xaa}
+		pubkey := BLSPubkey{0xaa}
 		binary.LittleEndian.PutUint64(pubkey[1:], i)
 		withdrawalCred := Root{0xbb}
 		binary.LittleEndian.PutUint64(withdrawalCred[1:], i)
@@ -22,10 +21,10 @@ func CreateTestValidators(count uint64, balance Gwei) []KickstartValidatorData {
 	return out
 }
 
-func CreateTestState(validatorCount uint64, balance Gwei) *FullFeaturedState {
-	out, err := KickStartState(Root{123}, 1564000000, CreateTestValidators(validatorCount, balance))
+func CreateTestState(validatorCount uint64, balance Gwei) (*BeaconStateView, *EpochsContext) {
+	out, epc, err := KickStartState(Root{123}, 1564000000, CreateTestValidators(validatorCount, balance))
 	if err != nil {
 		panic(err)
 	}
-	return out
+	return out, epc
 }

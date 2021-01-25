918075932d7e885382cc16d5192d97c50ce60349
protolambda <proto@protolambda.com>
2021-01-25 03:25:34 +0100

    gossip validation: attester slashings

--- eth2/beacon/indexed.go
+++ eth2/beacon/indexed.go
@@ -78,64 +78,83 @@ func (c *Phase0Config) IndexedAttestation() *ContainerTypeDef {
 	})
 }
 
-// Verify validity of slashable_attestation fields.
-func (spec *Spec) ValidateIndexedAttestation(epc *EpochsContext, state *BeaconStateView, indexedAttestation *IndexedAttestation) error {
+func ValidateIndexedAttestationIndicesSet(indexedAttestation *IndexedAttestation) (ValidatorSet, error) {
 	// wrap it in validator-sets. Does not sort it, but does make checking if it is a lot easier.
 	indices := ValidatorSet(indexedAttestation.AttestingIndices)
 
 	// Verify max number of indices
 	if count := uint64(len(indices)); count > spec.MAX_VALIDATORS_PER_COMMITTEE {
-		return fmt.Errorf("invalid indices count in indexed attestation: %d", count)
+		return nil, fmt.Errorf("invalid indices count in indexed attestation: %d", count)
+	}
+
+	// empty attestation
+	if len(indices) <= 0 {
+		return nil, errors.New("in phase 0 no empty attestation signatures are allowed")
 	}
 
 	// The indices must be sorted
 	if !sort.IsSorted(indices) {
-		return errors.New("attestation indices are not sorted")
+		return nil, errors.New("attestation indices are not sorted")
 	}
 
 	// Verify if the indices are unique. Simple O(n) check, since they are already sorted.
 	for i := 1; i < len(indices); i++ {
 		if indices[i-1] == indices[i] {
-			return fmt.Errorf("attestation indices at %d and %d are duplicate, both: %d", i-1, i, indices[i])
+			return nil, fmt.Errorf("attestation indices at %d and %d are duplicate, both: %d", i-1, i, indices[i])
 		}
+	}
+	return indices, nil
+}
+
+func (spec *Spec) ValidateIndexedAttestationNoSignature(state *BeaconStateView, indexedAttestation *IndexedAttestation) error {
+	indices, err := ValidateIndexedAttestationIndicesSet(indexedAttestation)
+	if err != nil {
+		return err
 	}
 
 	// Check the last item of the sorted list to be a valid index,
 	// if this one is valid, the others are as well, since they are lower.
-	if len(indices) > 0 {
-		valid, err := state.IsValidIndex(indices[len(indices)-1])
-		if err != nil {
-			return err
-		}
-		if !valid {
-			return errors.New("attestation indices contain out of range index")
-		}
+	valid, err := state.IsValidIndex(indices[len(indices)-1])
+	if err != nil {
+		return err
 	}
+	if !valid {
+		return errors.New("attestation indices contain out of range index")
+	}
+	return nil
+}
 
-	pubkeys := make([]*CachedPubkey, 0, 2)
-	for _, i := range indices {
-		pub, ok := epc.PubkeyCache.Pubkey(i)
+func ValidateIndexedAttestationSignature(dom BLSDomain, pubCache *PubkeyCache, indexedAttestation *IndexedAttestation) error {
+	pubkeys := make([]*CachedPubkey, 0, len(indexedAttestation.AttestingIndices))
+	for _, i := range indexedAttestation.AttestingIndices {
+		pub, ok := pubCache.Pubkey(i)
 		if !ok {
 			return fmt.Errorf("could not find pubkey for index %d", i)
 		}
 		pubkeys = append(pubkeys, pub)
 	}
-
-	// empty attestation
+	// empty attestation. (Double check, since this function is public, the user might not have validated if it's empty or not)
 	if len(pubkeys) <= 0 {
 		return errors.New("in phase 0 no empty attestation signatures are allowed")
 	}
 
-	dom, err := state.GetDomain(spec.DOMAIN_BEACON_ATTESTER, indexedAttestation.Data.Target.Epoch)
-	if err != nil {
-		return err
-	}
 	if !bls.FastAggregateVerify(pubkeys,
 		ComputeSigningRoot(indexedAttestation.Data.HashTreeRoot(tree.GetHashFn()), dom),
 		indexedAttestation.Signature,
 	) {
 		return errors.New("could not verify BLS signature for indexed attestation")
 	}
-
 	return nil
+}
+
+// Verify validity of slashable_attestation fields.
+func (spec *Spec) ValidateIndexedAttestation(epc *EpochsContext, state *BeaconStateView, indexedAttestation *IndexedAttestation) error {
+	if err := spec.ValidateIndexedAttestationNoSignature(state, indexedAttestation); err != nil {
+		return err
+	}
+	dom, err := state.GetDomain(spec.DOMAIN_BEACON_ATTESTER, indexedAttestation.Data.Target.Epoch)
+	if err != nil {
+		return err
+	}
+	return ValidateIndexedAttestationSignature(dom, epc.PubkeyCache, indexedAttestation)
 }
--- eth2/validation/gossip/attester_slashing.go
+++ eth2/validation/gossip/attester_slashing.go
@@ -2,10 +2,65 @@ package gossip
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/protolambda/zrnt/eth2/beacon"
 )
 
-func (gv *GossipValidator) ValidateAttesterSlashing(ctx context.Context, attSl *beacon.AttesterSlashing) GossipValidatorResult {
-	// TODO
+// Check if any of the indices has NOT been seen before, return true if so.
+// May not be an index within valid range.
+// It is up to the topic subscriber to mark indices as seen. Indices which are checked may not be valid,
+// and should not be marked as seen because of just the check itself.
+// It is recommended to regard any indices which were finalized as slashed, as seen.
+type AttesterSlashedAnySeenFn func(indices []beacon.ValidatorIndex) bool
+
+func (gv *GossipValidator) ValidateAttesterSlashing(ctx context.Context, attSl *beacon.AttesterSlashing, seenFn AttesterSlashedAnySeenFn) GossipValidatorResult {
+	sa1 := &attSl.Attestation1
+	sa2 := &attSl.Attestation2
+
+	// [REJECT] All of the conditions within process_attester_slashing pass validation.
+	// Part 1: just light checks, make sure the formatting is right, no signature checks yet.
+	if !beacon.IsSlashableAttestationData(&sa1.Data, &sa2.Data) {
+		return GossipValidatorResult{REJECT, errors.New("attester slashing has no valid reasoning")}
+	}
+	indices1, err := beacon.ValidateIndexedAttestationIndicesSet(sa1)
+	if err != nil {
+		return GossipValidatorResult{REJECT, errors.New("attestation 1 of attester slashing cannot be verified")}
+	}
+	indices2, err := beacon.ValidateIndexedAttestationIndicesSet(sa2)
+	if err != nil {
+		return GossipValidatorResult{REJECT, errors.New("attestation 2 of attester slashing cannot be verified")}
+	}
+
+	// [IGNORE] At least one index in the intersection of the attesting indices of each attestation has not yet been seen in any prior attester_slashing
+	slashable := make([]beacon.ValidatorIndex, 0, len(indices1))
+	indices1.ZigZagJoin(indices2, func(i beacon.ValidatorIndex) {
+		slashable = append(slashable, i)
+	}, nil)
+
+	if !seenFn(slashable) {
+		return GossipValidatorResult{IGNORE, errors.New("no unseen slashable attester indices")}
+	}
+
+	// [REJECT] All of the conditions within process_attester_slashing pass validation.
+	// Part 2: signature checks
+	dom1, err := gv.GetDomain(gv.Spec.DOMAIN_BEACON_ATTESTER, sa1.Data.Target.Epoch)
+	if err != nil {
+		return GossipValidatorResult{REJECT, fmt.Errorf("could not get attester BLS domain 1 for epoch %d: %v", sa1.Data.Target.Epoch, err)}
+	}
+	dom2, err := gv.GetDomain(gv.Spec.DOMAIN_BEACON_ATTESTER, sa2.Data.Target.Epoch)
+	if err != nil {
+		return GossipValidatorResult{REJECT, fmt.Errorf("could not get attester BLS domain 2 for epoch %d: %v", sa2.Data.Target.Epoch, err)}
+	}
+
+	// Note: the unrecognized indices in here are thrown away.
+	// TODO: get pubkey cache via EPC or not?
+	if err := beacon.ValidateIndexedAttestationSignature(dom1, nil, sa1); err != nil {
+		return GossipValidatorResult{REJECT, fmt.Errorf("attester slashing att 1 signature is invalid: %v", err)}
+	}
+	if err := beacon.ValidateIndexedAttestationSignature(dom2, nil, sa2); err != nil {
+		return GossipValidatorResult{REJECT, fmt.Errorf("attester slashing att 2 signature is invalid: %v", err)}
+	}
+
 	return GossipValidatorResult{ACCEPT, nil}
 }

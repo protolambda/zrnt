f09d80eb9c74c28f3b43ac40ed7944e00f4cd7a3
protolambda <proto@protolambda.com>
2021-05-22 13:06:59 +0200

    execution payload and sync aggregate tests

---
+++ tests/spec/test_runners/operations/sync_aggregate.go
@@ -0,0 +1,37 @@
+package operations
+
+import (
+	"context"
+	"fmt"
+	"github.com/protolambda/zrnt/eth2/beacon/altair"
+	"github.com/protolambda/zrnt/eth2/beacon/common"
+	"github.com/protolambda/zrnt/tests/spec/test_util"
+	"testing"
+)
+
+type SyncAggregateTestCase struct {
+	test_util.BaseTransitionTest
+	SyncAggregate altair.SyncAggregate
+}
+
+func (c *SyncAggregateTestCase) Load(t *testing.T, forkName test_util.ForkName, readPart test_util.TestPartReader) {
+	c.BaseTransitionTest.Load(t, forkName, readPart)
+	test_util.LoadSSZ(t, "sync_aggregate", c.Spec.Wrap(&c.SyncAggregate), readPart)
+}
+
+func (c *SyncAggregateTestCase) Run() error {
+	epc, err := common.NewEpochsContext(c.Spec, c.Pre)
+	if err != nil {
+		return err
+	}
+	s, ok := c.Pre.(*altair.BeaconStateView)
+	if !ok {
+		return fmt.Errorf("unrecognized state type: %T", c.Pre)
+	}
+	return altair.ProcessSyncCommittee(context.Background(), c.Spec, epc, s, &c.SyncAggregate)
+}
+
+func TestSyncAggregate(t *testing.T) {
+	test_util.RunTransitionTest(t, []test_util.ForkName{"altair"}, "operations", "sync_committee",
+		func() test_util.TransitionTest { return new(SyncAggregateTestCase) })
+}

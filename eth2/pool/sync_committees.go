b8e080b8916b5b245c675c3180d79179b88edfd6
protolambda <proto@protolambda.com>
2022-07-10 06:48:27 +0200

    todo sync committee message pool

---
+++ eth2/pool/sync_committees.go
@@ -0,0 +1,16 @@
+package pool
+
+import (
+	"context"
+
+	"github.com/protolambda/zrnt/eth2/beacon/altair"
+)
+
+type SyncCommitteePool struct {
+	// TODO
+}
+
+func (sp *SyncCommitteePool) AddSyncCommitteeMessage(ctx context.Context, msg *altair.SyncCommitteeMessage) error {
+	// TODO
+	return nil
+}

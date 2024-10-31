8c3791269037d9475ccb3ce7949cbd7474347b8d
Jay <jay@superblock.xyz>
2024-10-31 14:50:26 +0900

    remove eth1vote 3

---
+++ eth2/beacon/electra/transition.go
@@ -0,0 +1,107 @@
+package deneb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/protolambda/zrnt/eth2/beacon/altair"
+	"github.com/protolambda/zrnt/eth2/beacon/capella"
+	"github.com/protolambda/zrnt/eth2/beacon/common"
+	"github.com/protolambda/zrnt/eth2/beacon/deneb"
+	"github.com/protolambda/zrnt/eth2/beacon/phase0"
+)
+
+func (state *BeaconStateView) ProcessEpoch(ctx context.Context, spec *common.Spec, epc *common.EpochsContext) error {
+	vals, err := state.Validators()
+	if err != nil {
+		return err
+	}
+	flats, err := common.FlattenValidators(vals)
+	if err != nil {
+		return err
+	}
+	attesterData, err := altair.ComputeEpochAttesterData(ctx, spec, epc, flats, state)
+	if err != nil {
+		return err
+	}
+	just := phase0.JustificationStakeData{
+		CurrentEpoch:                  epc.CurrentEpoch.Epoch,
+		TotalActiveStake:              epc.TotalActiveStake,
+		PrevEpochUnslashedTargetStake: attesterData.PrevEpochUnslashedStake.TargetStake,
+		CurrEpochUnslashedTargetStake: attesterData.CurrEpochUnslashedTargetStake,
+	}
+	if err := phase0.ProcessEpochJustification(ctx, spec, &just, state); err != nil {
+		return err
+	}
+	if err := altair.ProcessInactivityUpdates(ctx, spec, attesterData, state); err != nil {
+		return err
+	}
+	if err := altair.ProcessEpochRewardsAndPenalties(ctx, spec, epc, attesterData, state); err != nil {
+		return err
+	}
+	// Modified in Deneb
+	if err := deneb.ProcessEpochRegistryUpdates(ctx, spec, epc, flats, state); err != nil {
+		return err
+	}
+	if err := phase0.ProcessEffectiveBalanceUpdates(ctx, spec, epc, flats, state); err != nil {
+		return err
+	}
+	if err := phase0.ProcessRandaoMixesReset(ctx, spec, epc, state); err != nil {
+		return err
+	}
+	if err := capella.ProcessHistoricalSummariesUpdate(ctx, spec, epc, state); err != nil {
+		return err
+	}
+	if err := altair.ProcessParticipationFlagUpdates(ctx, spec, state); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (state *BeaconStateView) ProcessBlock(ctx context.Context, spec *common.Spec, epc *common.EpochsContext, benv *common.BeaconBlockEnvelope) error {
+	body, ok := benv.Body.(*deneb.BeaconBlockBody)
+	if !ok {
+		return fmt.Errorf("unexpected block type %T in Bellatrix ProcessBlock", benv.Body)
+	}
+	expectedProposer, err := epc.GetBeaconProposer(benv.Slot)
+	if err != nil {
+		return err
+	}
+	if err := common.ProcessHeader(ctx, spec, state, &benv.BeaconBlockHeader, expectedProposer); err != nil {
+		return err
+	}
+	if err := capella.ProcessWithdrawals(ctx, spec, state, &body.ExecutionPayload); err != nil {
+		return err
+	}
+	// Modified in Deneb
+	eng, ok := spec.ExecutionEngine.(deneb.ExecutionEngine)
+	if !ok {
+		return fmt.Errorf("provided execution-engine interface does not support Deneb: %T", spec.ExecutionEngine)
+	}
+	if err := deneb.ProcessExecutionPayload(ctx, spec, state, body, eng); err != nil {
+		return err
+	}
+	if err := phase0.ProcessRandaoReveal(ctx, spec, epc, state, body.RandaoReveal); err != nil {
+		return err
+	}
+	// Safety checks, in case the user of the function provided too many operations
+	if err := body.CheckLimits(spec); err != nil {
+		return err
+	}
+
+	if err := phase0.ProcessProposerSlashings(ctx, spec, epc, state, body.ProposerSlashings); err != nil {
+		return err
+	}
+	if err := phase0.ProcessAttesterSlashings(ctx, spec, epc, state, body.AttesterSlashings); err != nil {
+		return err
+	}
+	// Modified in Deneb
+	if err := deneb.ProcessAttestations(ctx, spec, epc, state, body.Attestations); err != nil {
+		return err
+	}
+	// Modified in Deneb
+	if err := deneb.ProcessVoluntaryExits(ctx, spec, epc, state, body.VoluntaryExits); err != nil {
+		return err
+	}
+	return nil
+}

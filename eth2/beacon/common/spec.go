194f3c751fb79cb77bdc93486d44512878389d0f
Jay <jay@superblock.xyz>
2024-11-08 18:04:04 +0900

    added electra_fork_version selector

--- eth2/beacon/common/spec.go
+++ eth2/beacon/common/spec.go
@@ -321,7 +321,11 @@ func (spec *Spec) ForkVersion(slot Slot) Version {
 		return spec.ALTAIR_FORK_VERSION
 	} else if epoch < spec.CAPELLA_FORK_EPOCH {
 		return spec.BELLATRIX_FORK_VERSION
-	} else {
+	} else if epoch < spec.DENEB_FORK_EPOCH {
 		return spec.CAPELLA_FORK_VERSION
+	} else if epoch < spec.ELECTRA_FORK_EPOCH {
+		return spec.DENEB_FORK_VERSION
+	} else {
+		return spec.ELECTRA_FORK_VERSION
 	}
 }
